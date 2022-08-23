a04a5796cb5345cf910ebf6149571a4853f256a8
rch850 <rich850@gmail.com>
2022-08-24 08:07:04 +0900

    🚑 Prevent scroll error message on console

--- src/lib-scroll.ts
+++ src/lib-scroll.ts
@@ -1,7 +1,12 @@
 // https://stackoverflow.com/a/49853392/1081774
 
 export function preventDefault(e: Event) {
-  e.preventDefault();
+  // TODO: Improve scroll prevention
+  // In some case, cancelable is false and it cannot disable scroll .
+  // see: https://qiita.com/yusukeasari/items/a6433f47cdc6018a643f
+  if (e.cancelable) {
+    e.preventDefault();
+  }
 }
 
 export function disableScroll() {
